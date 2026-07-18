0d450b9d276f80982c04de4a01e044e66aac0749
mahmoud118-1279 <mahmoudfahmy118@gmail.com>
2026-07-18 15:51:25 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,11 +4,10 @@ import streamlit as st
 
 warnings.filterwarnings('ignore')
 
-# تحديد مسار الفولدر الحالي على الفلاشة ديناميكياً (ليعمل على ويندوز/ماك/لينكس)
+# تحديد مسار الفولدر الحالي على السيرفر
 BASE_DIR = Path(__file__).resolve().parent
 
 # الإعدادات العامة
-
 SYMBOL_FILE = BASE_DIR / "egx_symbols.csv"
 LEARNING_DATA_FILE = BASE_DIR / "ai_learning_data.json"
 
@@ -18,11 +17,7 @@ DEFAULT_TIME_FRAME = 365  # عدد أيام البيانات التاريخية
 # إعدادات تحليل الأخبار
 NEWS_MAX_RESULTS = 5  # عدد الأخبار التي يتم جلبها لكل شركة
 
-# إعدادات إشعارات تليجرام
-
-
-# قراءة البيانات السرية بأمان من إعدادات السيرفر السحابي
+# قراءة البيانات السرية بأمان من إعدادات السيرفر السحابي (Secrets)
 API_KEY = st.secrets["API_KEY"]
 TELEGRAM_TOKEN = st.secrets["TELEGRAM_TOKEN"]
 TELEGRAM_CHAT_ID = st.secrets["TELEGRAM_CHAT_ID"]
-TELEGRAM_CHAT_ID = "1687227481"
